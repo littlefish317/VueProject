4c81122bfbe6413f03730663b3c26711fa709e22
Katie Uhl <katie.uhl@hotmail.com>
2019-02-24 17:27:18 -0500

    KU rebase after cart is added

--- src/main.js
+++ src/main.js
@@ -1,20 +1,33 @@
+//Create a new button and method to decrement the value of `cart`.
+
 var app = new Vue({
-	el: '#app',
-	data: {
-		product: 'Socks',
-		image: './assets/vmSocks-green.jpg', 
-		inStock: true,
-		details: ["80% cotton", "20% polyester", "Gender-neutral"],
-		variants: [
-			{
-				variantId: 2234,
-				variantColor: "green"
-			},
-			{
-				variantId: 2235,
-				variantColor: "blue"
-			}
-		]
-		
-	}
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
+  }
 })
+
