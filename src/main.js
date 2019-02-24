b5d167fcb552d1a89892256aea6d8181632a1afc
Katie Uhl <katie.uhl@hotmail.com>
2019-02-24 17:19:50 -0500

    KU add foreach loops for lists

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ var app = new Vue({
 	data: {
 		product: 'Socks',
 		image: './assets/vmSocks-green.jpg', 
-		inventory: 0
+		inStock: true,
+		details: ["80% cotton", "20% polyester", "Gender-neutral"],
+		variants: [
+			{
+				variantId: 2234,
+				variantColor: "green"
+			},
+			{
+				variantId: 2235,
+				variantColor: "blue"
+			}
+		]
+		
 	}
 })
