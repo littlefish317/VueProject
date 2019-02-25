ab79569bea93fd8dfdebd1f7f7ea93ad742d84dd
Katie Uhl <katie.uhl@hotmail.com>
2019-02-24 19:20:17 -0500

    KU change cart to push ID

--- src/main.js
+++ src/main.js
@@ -39,9 +39,6 @@ Vue.component('product', {
           Add to cart
           </button>
 
-          <div class="cart">
-            <p>Cart({{ cart }})</p>
-          </div>
 
        </div>  
     
@@ -66,13 +63,12 @@ Vue.component('product', {
             variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
             variantQuantity: 0     
           }
-        ],
-        cart: 0
+        ]
     }
   },
     methods: {
       addToCart: function() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
       },
       updateProduct: function(index) {  
           this.selectedVariant = index
@@ -100,6 +96,12 @@ Vue.component('product', {
 var app = new Vue({
     el: '#app',
     data: {
-      premium: true
+      premium: true,
+      cart: []
+    },
+    methods: {
+    	updateCart: function(id) {
+          this.cart.push(id)
+      },
     }
 })
